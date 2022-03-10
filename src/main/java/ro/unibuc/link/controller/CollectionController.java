d1164a8394037fad92b1c8c954187a80befe0886
Onea Radu <onearadu12@gmail.com>
2022-03-10 22:34:16 +0200

    Made collection redirect to work and worked and some bugs. (#6)

--- src/main/java/ro/unibuc/link/controller/CollectionController.java
+++ src/main/java/ro/unibuc/link/controller/CollectionController.java
@@ -3,7 +3,6 @@ package ro.unibuc.link.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import ro.unibuc.link.data.CollectionEntity;
 import ro.unibuc.link.dto.*;
 import ro.unibuc.link.services.CollectionService;
 
@@ -11,35 +10,42 @@ import ro.unibuc.link.services.CollectionService;
 @RequestMapping("/collection")
 public class CollectionController {
     @Autowired
-    private CollectionService CollectionService;
+    private CollectionService collectionService;
 
     @GetMapping("/check/{collectionName}")
     public @ResponseBody
     IsAvailableDTO checkIfCollectionIsAvailable(@PathVariable String collectionName) {
-        return CollectionService.checkCollectionNameIsAvailable(collectionName);
+        return collectionService.checkCollectionNameIsAvailable(collectionName);
     }
 
     @PostMapping("/set")
     public @ResponseBody
     CollectionShowDTO setMapping(@RequestBody CollectionSetDTO collectionSetDTO) {
-        return CollectionService.setCollection(collectionSetDTO);
+        return collectionService.setCollection(collectionSetDTO);
     }
 
     @DeleteMapping("")
     public @ResponseBody
     CollectionShowDTO deleteMapping(@RequestBody CollectionDeleteDTO collectionDeleteDTO) {
-        return CollectionService.deleteCollection(collectionDeleteDTO.getCollectionName(), collectionDeleteDTO.getPrivateWord());
+        return collectionService.deleteCollection(collectionDeleteDTO.getCollectionName(), collectionDeleteDTO.getPrivateWord());
     }
 
     @PostMapping("/add/{collectionName}")
     public @ResponseBody
     CollectionShowDTO addUrl(@PathVariable String collectionName, @RequestBody UrlCollectionDTO urlCollectionDTO) {
-        return CollectionService.addUrlToCollection(collectionName, urlCollectionDTO.getUrlCollectionEntity(), urlCollectionDTO.getPrivateWord());
+        return collectionService.addUrlToCollection(collectionName, urlCollectionDTO.getUrlCollectionEntity(), urlCollectionDTO.getPrivateWord());
     }
 
     @DeleteMapping("/remove/{collectionName}")
     public @ResponseBody
     CollectionShowDTO removeUrl(@PathVariable String collectionName, @RequestBody UrlCollectionDTO urlCollectionDTO) {
-        return CollectionService.removeUrlFromCollection(collectionName, urlCollectionDTO.getUrlCollectionEntity(), urlCollectionDTO.getPrivateWord());
+        return collectionService.removeUrlFromCollection(collectionName, urlCollectionDTO.getUrlCollectionEntity(), urlCollectionDTO.getPrivateWord());
     }
+
+
+    @GetMapping("/redirect/{collectionName}/{url}")
+    public String getRedirectMapping(@PathVariable String collectionName, @PathVariable String url) {
+        return collectionService.getRedirectMapping(collectionName, url);
+    }
+
 }
--- src/main/java/ro/unibuc/link/dto/CollectionSetDTO.java
+++ src/main/java/ro/unibuc/link/dto/CollectionSetDTO.java
@@ -1,13 +1,15 @@
 package ro.unibuc.link.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class CollectionSetDTO {
     private String collectionName;
     private String privateWord;
 
-    public CollectionSetDTO(String collectionName, String privateWord){
+    public CollectionSetDTO(String collectionName, String privateWord) {
         this.collectionName = collectionName;
         this.privateWord = privateWord;
     }
